f4f91938fa44d8233ee51a64530054e260e2e5ff
Nicholas Gower <nicholasgower01@gmail.com>
2025-07-13 12:01:19 -0700

    Disabled existing music.

--- prototypes/sound/music.lua
+++ prototypes/sound/music.lua
@@ -1,16 +1,16 @@
 if settings.startup["disable-muluna-music"].value == false then
-    data:extend{
-        {
-        name = "muluna-sandy-boys-beth-cohens",
-        type = "ambient-sound",
-        track_type = "main-track",
-        planet = "muluna",
-        sound = {
-            filename = "__muluna-graphics__/sound/music/1 - Sandy Boys Beth Cohen's [aiqbWEikr4w].ogg",
-            volume = 0.5,
-        }
-    }
-    }  
+    -- data:extend{
+    --     {
+    --     name = "muluna-sandy-boys-beth-cohens",
+    --     type = "ambient-sound",
+    --     track_type = "main-track",
+    --     planet = "muluna",
+    --     sound = {
+    --         filename = "__muluna-graphics__/sound/music/1 - Sandy Boys Beth Cohen's [aiqbWEikr4w].ogg",
+    --         volume = 0.5,
+    --     }
+    -- }
+    -- }  
 end
 
 PlanetsLib.borrow_music("space-platform", data.raw["planet"]["muluna"])
