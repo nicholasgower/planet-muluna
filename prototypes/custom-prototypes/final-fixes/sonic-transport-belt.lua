d4bcd554678ead117f773097277789a391a61df0
Nicholas Gower <nicholasgower01@gmail.com>
2025-07-20 01:18:51 -0700

    Setup for sonic transport belt entity type, hopefully I never make it.

---
+++ prototypes/custom-prototypes/final-fixes/sonic-transport-belt.lua
@@ -0,0 +1,17 @@
+-- THIS IS SCOPE CREEP
+
+-- Muluna.constants.sonic_transport_belts = {}
+-- local sonic_transport_belts = Muluna.constants.sonic_transport_belts 
+-- for _,belt in pairs(data.raw["sonic-transport-belt"]) do
+--     belt.type = "transport-belt"
+
+--     data:extend{belt}
+--     sonic_transport_belts[belt.name] = {
+--         name = belt.name,
+--         no_belt_mixing = true,
+--         no_turns = true,
+--         no_splitters = true,
+--         no_undergrounds = true,
+--     }
+
+-- end
