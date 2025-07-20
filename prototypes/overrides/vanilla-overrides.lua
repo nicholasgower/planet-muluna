ada352566848aa4bc5c91e026e2360c68caf4ac8
Nicholas Gower <nicholasgower01@gmail.com>
2025-07-20 00:24:32 -0700

    Moved execution order of crusher changes to go first.

---
+++ prototypes/overrides/vanilla-overrides.lua
@@ -0,0 +1,19 @@
+local rro = Muluna.rro
+for _,tech in pairs(data.raw["technology"]) do
+    if tech.prerequisites then
+        rro.replace(tech.prerequisites,"space-platform-thruster","space-science-pack")
+    end
+    
+end
+
+data.raw["recipe"]["crusher"].subgroup = "smelting-machine"
+data.raw["item"]["crusher"].subgroup = "smelting-machine"
+data.raw["assembling-machine"]["crusher"].subgroup = "smelting-machine"
+data.raw["item"]["crusher"].order = "ca[crusher]"
+table.insert(data.raw["technology"]["recycling"].effects,
+    {
+                type = "unlock-recipe",
+                recipe = "muluna-regolith-recycling"
+    }
+
+)
--- prototypes/technology/dependencies-updates.lua
+++ prototypes/technology/dependencies-updates.lua
@@ -1,24 +1,7 @@
 local rro=Muluna.rro
 
 
-for _,tech in pairs(data.raw["technology"]) do
-    if tech.prerequisites then
-        rro.replace(tech.prerequisites,"space-platform-thruster","space-science-pack")
-    end
-    
-end
 
-data.raw["recipe"]["crusher"].subgroup = "smelting-machine"
-data.raw["item"]["crusher"].subgroup = "smelting-machine"
-data.raw["assembling-machine"]["crusher"].subgroup = "smelting-machine"
-data.raw["item"]["crusher"].order = "ca[crusher]"
-table.insert(data.raw["technology"]["recycling"].effects,
-    {
-                type = "unlock-recipe",
-                recipe = "muluna-regolith-recycling"
-    }
-
-)
 
 --replace_object(data.raw["technology"]["space-science-pack"].prerequisites,"space-platform-thruster","space-platform")
 rro.replace(data.raw["technology"]["space-science-pack"].prerequisites,"space-platform","muluna-alice-propellant")
