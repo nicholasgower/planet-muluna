7066026e2b9227709979ce9b61cdf03ba0fb7027
Nicholas Gower <nicholasgower01@gmail.com>
2025-07-22 22:02:28 -0700

    Added migration removing uranium/stone spawning settings from Muluna.

---
+++ migrations/muluna-2-resources_.lua
@@ -0,0 +1,22 @@
+game.print("test")
+local function remove_resource(resource) 
+    local map_gen_settings = game.planets.muluna.surface.map_gen_settings
+    map_gen_settings.autoplace_controls[resource] = nil
+    map_gen_settings.autoplace_settings.entity.settings[resource] = nil
+    game.planets.muluna.surface.map_gen_settings = map_gen_settings
+    game.planets.muluna.surface.regenerate_entity(resource)
+end
+if game.planets.muluna.surface then
+    remove_resource("uranium-ore")
+    remove_resource("stone")
+    --regenerate_resource("helium")
+end
+
+
+
+
+-- for surface in pairs(game.surfaces) do
+--     if surface.name == "muluna" then
+        
+--     end
+-- end
