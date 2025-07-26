7d90fec9e0c9026cf3d1b8c43874e4662da52566
Nicholas Gower <nicholasgower01@gmail.com>
2025-07-26 14:57:53 -0700

    Changed release script version number

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=planet-muluna_1.99.2/ -o planet-muluna_1.99.2.zip HEAD
+git archive --prefix=planet-muluna_1.99.3/ -o planet-muluna_1.99.3.zip HEAD
