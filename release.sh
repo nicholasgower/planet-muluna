9b2f7835f8caf77f747af22f1a35f7973a9f2c16
Nicholas Gower <nicholasgower01@gmail.com>
2025-07-27 18:07:32 -0700

    Update release.sh

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=planet-muluna_1.99.3/ -o planet-muluna_1.99.3.zip HEAD
+git archive --prefix=planet-muluna_1.99.4/ -o planet-muluna_1.99.4.zip HEAD
