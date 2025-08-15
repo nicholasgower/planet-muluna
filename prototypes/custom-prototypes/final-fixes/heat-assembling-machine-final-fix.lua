23a4662cdd22301d513edf450e109cf2e90c284e
Nicholas Gower <nicholasgower01@gmail.com>
2025-08-15 13:33:14 -0700

    Changed selection priority of heat assembling machine to fix layering issues.

--- prototypes/custom-prototypes/final-fixes/heat-assembling-machine-final-fix.lua
+++ prototypes/custom-prototypes/final-fixes/heat-assembling-machine-final-fix.lua
@@ -49,6 +49,7 @@ if data.raw["heat-assembling-machine"] then
         end
         reactor.neighbour_bonus = 0 or reactor.neighbour_bonus
         reactor.selection_box = flib_bounding_box.resize(assembler.selection_box,0.0)
+        reactor.selection_priority=49 --Default is 50
         assembler.selection_box = flib_bounding_box.resize(assembler.selection_box,-0.2)
         reactor.circuit_connector = table.deepcopy(assembler.circuit_connector)
         reactor.default_temperature_signal = data.raw["reactor"]["heating-tower"].default_temperature_signal
