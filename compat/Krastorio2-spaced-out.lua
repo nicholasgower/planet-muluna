b47a75fd6a48f9b21dae79ac1151c43f1ca4d8e9
Nicholas Gower <nicholasgower01@gmail.com>
2025-08-18 22:19:55 -0700

    compat: Krastorio2 Spaced Out: Rare metal ore is a potential product of regolith digging, currently with a 2.5% chance of being produced. This value can be tuned in the future if demand for it exists. https://github.com/nicholasgower/planet-muluna/issues/246

--- compat/Krastorio2-spaced-out.lua
+++ compat/Krastorio2-spaced-out.lua
@@ -8,7 +8,8 @@ if mods["Krastorio2-spaced-out"] then
     rro.replace_field(data.raw["technology"]["kr-singularity-lab"].effects,"recipe","biolab","cryolab")
     rro.soft_insert(data.raw["technology"]["kr-singularity-lab"].prerequisites,"cryolab")
 
-
+    
+    table.insert(data.raw["recipe"]["muluna-regolith-digging"].results,{type = "item", name = "kr-rare-metal-ore",amount = 1,probability = 0.025})
 
 
 
