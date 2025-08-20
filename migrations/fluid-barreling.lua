d8f224f15a42e6b7eefa07ad638f15f3764229ee
Nicholas Gower <nicholasgower01@gmail.com>
2025-08-20 00:32:15 -0700

    fix: Fixed crash when loading existing Muluna start save.

--- migrations/fluid-barreling.lua
+++ migrations/fluid-barreling.lua
@@ -1,5 +1,5 @@
 for _,force in pairs(game.forces) do
-    if force.technologies["fluid-handling"].researched == true then
+    if force.technologies["fluid-handling"] and force.technologies["fluid-handling"].researched == true then
         force.technologies["fluid-barreling"].researched = true
     end
 end
