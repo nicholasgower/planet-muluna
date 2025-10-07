d6a785ff389e13c65dbb87366fd95bc02131186d
Nicholas Gower <nicholasgower01@gmail.com>
2025-10-07 16:14:38 -0700

    graphics: Added more subtle pipe covers for the Vacuum heating tower. By Talandar.

--- prototypes/entity/vacuum-heating-tower.lua
+++ prototypes/entity/vacuum-heating-tower.lua
@@ -1,45 +1,4 @@
-local function vacuumheatingtowerpipepictures()
-  return
-  {
-    north =
-    {
-      filename = "__muluna-graphics__/graphics/pipe_covers/long/pipe-NN.png",
-      priority = "extra-high",
-      width = 71,
-      height = 38,
-      shift = util.by_pixel(2.25, 13.5),
-      scale = 0.5,
-      draw_as_shadow = true,
-    },
-    east =
-    {
-      filename = "__muluna-graphics__/graphics/pipe_covers/cut/pipe-E.png",
-      priority = "extra-high",
-      width = 42,
-      height = 76,
-      shift = util.by_pixel(-24.5, 1),
-      scale = 0.5
-    },
-    south =
-    {
-      filename = "__muluna-graphics__/graphics/pipe_covers/cut/pipe-S.png",
-      priority = "extra-high",
-      width = 88,
-      height = 61,
-      shift = util.by_pixel(0, -31.25),
-      scale = 0.5
-    },
-    west =
-    {
-      filename = "__muluna-graphics__/graphics/pipe_covers/cut/pipe-W.png",
-      priority = "extra-high",
-      width = 39,
-      height = 73,
-      shift = util.by_pixel(25.75, 1.25),
-      scale = 0.5
-    }
-  }
-end
+
 
 
 local recipe_category = "muluna-vacuum-heating-tower"
@@ -272,7 +231,7 @@ heating_boiler.fluid_boxes = {
             {
                 volume = 200,
                 pipe_covers = pipecoverspictures(),
-                pipe_picture = vacuumheatingtowerpipepictures(),
+                pipe_picture = Muluna.pipe_pictures.vacuumheatingtowerpipepictures(),
                 pipe_connections =
                 {
                 {flow_direction = "input-output", direction = defines.direction.west, position = {-1.0, 1}},
@@ -294,7 +253,7 @@ heating_boiler.fluid_boxes = {
             {
                 volume = 200,
                 pipe_covers = pipecoverspictures(),
-                pipe_picture = vacuumheatingtowerpipepictures(),
+                pipe_picture =  Muluna.pipe_pictures.vacuumheatingtowerpipepictures(),
                 pipe_connections =
                 {
                 {flow_direction = "input-output", direction = defines.direction.west, position = {-1.0, -1}},
