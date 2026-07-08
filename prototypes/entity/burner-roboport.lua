e17d87b2aafec6c4fd4900a2d0b57d7a1ff5774f
Nicholas Gower <nicholasgower01@gmail.com>
2026-07-08 15:53:18 -0700

    Burner roboport consumes no energy passively

--- prototypes/entity/burner-roboport.lua
+++ prototypes/entity/burner-roboport.lua
@@ -1,6 +1,7 @@
 local roboport = table.deepcopy(data.raw["roboport"]["roboport"])
 
 roboport.name = "muluna-burner-roboport"
+roboport.energy_usage = "0W"
 roboport.is_vacuum_roboport = true
 roboport.recharging_animation.filename = "__muluna-graphics__/graphics/entity/burner-roboport/burner-roboport-recharging.png"
 roboport.base.layers[1].filename = "__muluna-graphics__/graphics/entity/burner-roboport/burner-roboport-base.png"
