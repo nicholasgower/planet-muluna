613343b7b3c8a9eddcebba8f239437480a95c300
Nicholas Gower <nicholasgower01@gmail.com>
2026-07-16 12:46:35 -0700

    feat: Added "Asteroid upcycling" recipes, a collection of recipes that consume astronomical data and asteroid chunks in exchange for higher quality asteroid chunks.

---
+++ prototypes/recipe/asteroid-upcycling.lua
@@ -0,0 +1,30 @@
+
+
+local asteroid_list={"metallic-asteroid","carbonic-asteroid","oxide-asteroid"}
+local quality_chance = 0.15 --Chance of upgrading the quality of an asteroid 
+local loss_chance = 0.2 -- Chance of a craft being a complete loss
+for _,asteroid_name in pairs(asteroid_list) do
+    local asteroid = asteroid_name .. "-chunk"
+    local new_recipe = Muluna.rro.merge(data.raw["recipe"]["metallic-asteroid-reprocessing"],{
+        name = "muluna-" .. asteroid .. "-upcycling",
+        
+        type = "recipe",
+        categories ={"muluna-crusher-2"},
+        icons =  Muluna.icons.dual_icon(asteroid,"muluna-astronomical-data"),
+        ingredients = {
+            {type = "item",name = asteroid,amount=1},
+            {type = "fluid", name = "muluna-astronomical-data",amount=1,fluidbox_index=30}
+        },
+        results = {
+            {type ="item",name = asteroid,amount=1,shared_probability={min = loss_chance,max = 1-quality_chance}},
+            {type ="item",name = asteroid,amount=1,shared_probability={min = 1-quality_chance,max = 1},quality_change = 1},
+        },
+        allow_quality=true,
+        enabled = false,
+    })
+    data:extend{new_recipe}
+    table.insert(data.raw["technology"]["muluna-asteroid-upcycling"].effects, {
+        type = "unlock-recipe",
+        recipe = new_recipe.name
+    })
+end
