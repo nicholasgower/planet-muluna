bcdbabfedb8b179264f2a87ea9bca536698b8d69
Nicholas Gower <nicholasgower01@gmail.com>
2026-07-16 13:07:35 -0700

    Adjusted order of upcycling recipes.

--- prototypes/recipe/asteroid-upcycling.lua
+++ prototypes/recipe/asteroid-upcycling.lua
@@ -3,7 +3,9 @@
 local asteroid_list={"metallic-asteroid","carbonic-asteroid","oxide-asteroid"}
 local quality_chance = 0.15 --Chance of upgrading the quality of an asteroid 
 local loss_chance = 0.2 -- Chance of a craft being a complete loss
+local i = 0
 for _,asteroid_name in pairs(asteroid_list) do
+    i = i + 1
     local asteroid = asteroid_name .. "-chunk"
     local new_recipe = Muluna.rro.merge(data.raw["recipe"]["metallic-asteroid-reprocessing"],{
         name = "muluna-" .. asteroid .. "-upcycling",
@@ -19,6 +21,7 @@ for _,asteroid_name in pairs(asteroid_list) do
             {type ="item",name = asteroid,amount=1,shared_probability={min = loss_chance,max = 1-quality_chance}},
             {type ="item",name = asteroid,amount=1,shared_probability={min = 1-quality_chance,max = 1},quality_change = 1},
         },
+        order = data.raw["recipe"]["oxide-asteroid-reprocessing"].order .. "-" ..  tostring(i),
         allow_quality=true,
         enabled = false,
     })
