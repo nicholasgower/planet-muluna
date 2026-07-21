0012df08052ebef7f50d9ef6aacdd1e34e250718
Nicholas Gower <nicholasgower01@gmail.com>
2026-07-21 14:04:27 -0700

    Bumped version to 2.2.102

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=planet-muluna_2.2.101/ -o planet-muluna_2.2.101.zip HEAD
+git archive --prefix=planet-muluna_2.2.102/ -o planet-muluna_2.2.102.zip HEAD
