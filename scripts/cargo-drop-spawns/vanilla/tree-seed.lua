a3af5e0926d29bb6e3b21b89194592f6a903d4f1
Nicholas Gower <nicholasgower01@gmail.com>
2026-07-24 12:47:45 -0700

    balance: Increased the number of tree seeds included with initial cargo drops.

--- scripts/cargo-drop-spawns/vanilla/tree-seed.lua
+++ scripts/cargo-drop-spawns/vanilla/tree-seed.lua
@@ -2,5 +2,5 @@
 return {
         pod_count = function() return 4 end,
         item = "tree-seed",
-        item_quantity = function() return math.random(2,6)+math.random(3,7) end,
+        item_quantity = function() return 10 + math.random(2,6)+math.random(3,7) end,
     }
