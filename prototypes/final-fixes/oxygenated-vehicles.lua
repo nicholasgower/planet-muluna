911343e05b9dec5aab91e62dda3560ea71b67d7e
Nicholas Gower <nicholasgower01@gmail.com>
2026-07-26 16:14:14 -0700

    Split nuclear fuel category.

--- prototypes/final-fixes/oxygenated-vehicles.lua
+++ prototypes/final-fixes/oxygenated-vehicles.lua
@@ -1,20 +1,42 @@
 local surface_property_lib = require("__PlanetsLib__.lib.surface-property-lib")
-for _,vehicle in pairs(data.raw.car) do
-    if (not vehicle.PlanetsLib_do_not_generate_variants) and surface_property_lib.fits_surface_conditions(vehicle,"muluna") and vehicle.energy_source.type == "burner" then
-        local new_energy_source = table.deepcopy(vehicle.energy_source)
-        new_energy_source.fuel_categories = {"muluna-oxygenated-fuel"}
-        new_energy_source.burnt_inventory_size = new_energy_source.burnt_inventory_size or 2
-        vehicle.fast_replaceable_group = not vehicle.fast_replaceable_group and vehicle.name
-        local new_vehicle = PlanetsLib.create_planet_entity_variant("muluna",vehicle,
-            {
-                
-                energy_source = new_energy_source,
-                placeable_by = data.raw.item[vehicle.name] and {{item = vehicle.name, count =1}}
-            },
-            "muluna-runtime-vehicle-replacement"
-        )
-    end
-    
-    
+local categories = {"car"}
 
+local nuclear_fuel_category = data.raw["item"]["nuclear-fuel"].fuel_category
+local new_nuclear_fuel_category = "muluna-nuclear-chemical-fuel"
+data.raw["item"]["nuclear-fuel"].fuel_category=new_nuclear_fuel_category
+
+for _,category in pairs(categories) do
+    for _,vehicle in pairs(data.raw[category]) do
+        if (not vehicle.PlanetsLib_do_not_generate_variants) and 
+        vehicle.surface_conditions and surface_property_lib.fits_surface_conditions(vehicle,"muluna") and 
+        vehicle.energy_source.type == "burner" and 
+        vehicle.energy_source.fuel_categories and Muluna.rro.contains(vehicle.energy_source.fuel_categories,"chemical") then
+            local new_energy_source = table.deepcopy(vehicle.energy_source)
+            new_energy_source.fuel_categories = {"muluna-oxygenated-fuel",new_nuclear_fuel_category}
+            new_energy_source.burnt_inventory_size = new_energy_source.burnt_inventory_size or 2
+            vehicle.fast_replaceable_group = not vehicle.fast_replaceable_group and vehicle.name
+            local new_vehicle = PlanetsLib.create_planet_entity_variant("muluna",vehicle,
+                {
+                    
+                    energy_source = new_energy_source,
+                    placeable_by = data.raw.item[vehicle.name] and {{item = vehicle.name, count =1}}
+                },
+                "muluna-runtime-vehicle-replacement"
+            )
+        end
+        
+        
+
+    end
 end
+
+
+for _,entity in pairs(Muluna.flib_prototypes.all("entity")) do
+    local energy_source = entity.energy_source 
+    if energy_source and energy_source.type == "burner" then
+        if Muluna.rro.contains(energy_source.fuel_categories,nuclear_fuel_category) then
+            table.insert(energy_source.fuel_categories,new_nuclear_fuel_category)
+        end
+    end
+end
+
--- prototypes/fuel-categories.lua
+++ prototypes/fuel-categories.lua
@@ -3,5 +3,11 @@ data:extend {
         type = "fuel-category",
         name = "muluna-oxygenated-fuel",
         
+    },
+    {
+        type = "fuel-category",
+        name = "muluna-nuclear-chemical-fuel",
+        --localised_name = {"fuel-category-name.muluna-oxygenated-fuel"}
+        
     }
 }
