0c8486ec938ae4bae9abdc1626132827c0d47d28
Nicholas Gower <nicholasgower01@gmail.com>
2026-07-26 16:18:20 -0700

    other: Rocket buggy now supports logistic requests.

--- prototypes/entity/lunar-buggy.lua
+++ prototypes/entity/lunar-buggy.lua
@@ -26,6 +26,7 @@ buggy.icons = {
 }
 
 
+buggy.trash_inventory_size = data.raw["car"]["tank"].trash_inventory_size
 buggy.equipment_grid = data.raw["car"]["tank"].equipment_grid
 buggy.allow_remote_driving = true
 buggy.collision_box = {{-0.7*buggy_scale, -1*buggy_scale}, {0.7*buggy_scale, 1*buggy_scale}}
