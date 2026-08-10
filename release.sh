77db66609011790f874fd840cf38d1e2ecf48017
Nicholas Gower <nicholasgower01@gmail.com>
2026-08-09 18:47:11 -0700

    Raised version to 2.2.106 and updated changelog

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=planet-muluna_2.2.105/ -o planet-muluna_2.2.105.zip HEAD
+git archive --prefix=planet-muluna_2.2.106/ -o planet-muluna_2.2.106.zip HEAD
