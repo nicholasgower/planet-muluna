a7742e92e28a0a4e1889f7bbcc7d29e2b98c3edb
Nicholas Gower <nicholasgower01@gmail.com>
2026-08-12 12:31:49 -0700

    balance: Reworked regolith digging recipe to use shared probability. Rigor modules decrease the odds of producing crushed stone and increase the odds of producing everything else.

--- prototypes/entity/ground-digger.lua
+++ prototypes/entity/ground-digger.lua
@@ -9,19 +9,20 @@ Muluna:extend {{
     type = "recipe",
     name = "muluna-regolith-digging",
     categories = {"ground-digging"},
-    energy_required = 2.5,
+    energy_required = 2,
     ingredients = {},
     results = {
-        {type = "item", name = "muluna-lunar-regolith", amount = 1,independent_probability = 0.25},
-        {type = "item", name = "stone-crushed", amount = 1}
+        {type = "item", name = "muluna-lunar-regolith", amount = 1,shared_probability = {min = 0, max = 0.2}},
+        {type = "item", name = "stone-crushed", amount = 1,shared_probability = {min = 0.2, max = 1},rigor_product = true}
     },
+    rigor_invert = true,
     enabled = false,
     show_amount_in_title = false,
     allow_decomposition = false,
     allow_productivity = true,
     maximum_productivity = 9999999,
     main_product = "muluna-lunar-regolith",
-    rigor_no_results=true,
+    --rigor_no_results=true,
     auto_recycle = false,
     rigor_whitelist=mods["rigor-module"] and true
     --localised_name = {"item-name.sand"},
