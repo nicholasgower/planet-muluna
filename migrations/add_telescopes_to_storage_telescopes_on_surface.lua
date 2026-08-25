4343f57e16acc3f4ee159446177b3e6aa05c69ce
Nicholas Gower <nicholasgower01@gmail.com>
2026-08-24 18:32:33 -0700

    fix: Fixed migration when telescopes are invalid.

--- migrations/add_telescopes_to_storage_telescopes_on_surface.lua
+++ migrations/add_telescopes_to_storage_telescopes_on_surface.lua
@@ -1,7 +1,12 @@
 if not storage.telescopes_on_surface then storage.telescopes_on_surface = {} end
 for i,telescope in pairs(storage.telescopes) do
     local entity = telescope["assembling-machine"]
-    if not storage.telescopes_on_surface[entity.surface.name] then storage.telescopes_on_surface[entity.surface.name] = {} end
-    storage.telescopes_on_surface[entity.surface.name][entity.unit_number] = true
-    Muluna.update_telescope_daytime(telescope)
+    if entity.valid then 
+        if not storage.telescopes_on_surface[entity.surface.name] then storage.telescopes_on_surface[entity.surface.name] = {} end
+        storage.telescopes_on_surface[entity.surface.name][entity.unit_number] = true
+        Muluna.update_telescope_daytime(telescope)
+    else
+        storage.telescopes[i] = nil
+    end
+    
 end
