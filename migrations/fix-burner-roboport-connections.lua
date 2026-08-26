2f07f7360fbbfefdd80634656eee6e3c4159a9f2
Nicholas Gower <nicholasgower01@gmail.com>
2026-08-26 13:59:12 -0700

    fix: Fixed migration from 2.0 saves.

--- migrations/fix-burner-roboport-connections.lua
+++ migrations/fix-burner-roboport-connections.lua
@@ -1,4 +1,4 @@
-for _,burner_roboport_data in pairs(storage.burner_roboports) do
+for _,burner_roboport_data in pairs(storage.burner_roboports or {}) do
     local roboport = burner_roboport_data.roboport
     roboport.update_connections()
 end
